4a57a951f1d8da7a6b7bc1b5c32d12905fa08319
dan-amzulescu-port <dan.a@getport.io>
2024-12-18 13:09:51 -0600

    Update port.py

--- .github/workflows/port.py
+++ .github/workflows/port.py
@@ -28,9 +28,9 @@ def get_token(client_id, client_secret):
     data = {"clientId": client_id, "clientSecret": client_secret}
     response = send_post_request(url, {"Content-Type": "application/json"}, data)
 
-    env_port_context = os.environ.get('ENV_PORT_CONTEXT', None).strip('"').replace('\\"', '"')
-
-    logging.info(f"value of cleaned_input: {env_port_context["inputs"]["requires_s_3"]}")
+    env_port_context = os.environ.get('ENV_PORT_CONTEXT', None)
+    context_input = env_port_context.strip('"').replace('\\"', '"')
+    logging.info(f"context_input: {type(context_input)}:{context_input}")
 
     if response is None:
         return None
