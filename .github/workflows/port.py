f34e748c4841c9fef962fc9c351ae2705f102ada
dan-amzulescu-port <dan.a@getport.io>
2024-12-18 11:26:35 -0600

    z

--- .github/workflows/port.py
+++ .github/workflows/port.py
@@ -29,6 +29,10 @@ def get_token(client_id, client_secret):
     response = send_post_request(url, {"Content-Type": "application/json"}, data)
 
     env_input = os.environ.get('JSON_INPUT')
+    logging.info(f"Type of env_input: {type(env_input)}")
+    first_parse = json.loads(env_input)
+    logging.info(f"Type of first_parse: {type(first_parse)}")
+    
     logging.info(f"JSON_INPUT: {env_input}")
     parsed_input = json.loads(env_input)
     logging.info(f'{parsed_input["inputs"]["requires_s3"]}')
