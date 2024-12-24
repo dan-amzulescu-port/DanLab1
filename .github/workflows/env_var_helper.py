27dba2adee3ad0c5c9a5e7e18956b054794926ef
dan-amzulescu-port <dan.a@getport.io>
2024-12-24 11:37:11 -0600

    1

--- .github/workflows/env_var_helper.py
+++ .github/workflows/env_var_helper.py
@@ -1,10 +1,8 @@
 import json
 import logging
 import os
-from datetime import datetime, timedelta
-from typing import Optional
 
-import pytz
+from typing import Optional
 
 
 def get_port_context():
@@ -26,25 +24,6 @@ def get_port_context():
     except Exception as e:
         logging.critical(f"Unexpected error: {e}")
         return None
-
-def calculate_time_delta(time_input: str) -> str:
-
-    current_time = datetime.now(pytz.UTC).replace(microsecond=0)
-
-    # Parse and add time delta
-    if time_input == "1 Day":
-        future_time = current_time + timedelta(days=1)
-    elif time_input == "2 Hours":
-        future_time = current_time + timedelta(hours=2)
-    elif time_input == "1 Week":
-        future_time = current_time + timedelta(weeks=1)
-    elif time_input == "Indefinite":
-        future_time = current_time + timedelta(days=365)
-    else:
-        raise ValueError(f"Unsupported time input: {time_input}")
-
-    # Convert to ISO 8601 format with milliseconds and Z for UTC
-    return future_time.isoformat().replace('+00:00', '.000Z')
 
 def get_env_var(var_name: str) -> Optional[str]:
     try:
